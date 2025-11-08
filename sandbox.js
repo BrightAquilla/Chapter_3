9be5104535bac6087f347475e4efb08f6441429f
BrightAquilla <aquillabright@gmail.com>
2025-11-08 13:32:01 +0300

    Add control structures and basic functions

---
+++ sandbox.js
@@ -0,0 +1,40 @@
+
+/*
+//for loop
+for(let i= 0; i< 5; i++){
+    console.log('in loop:', i);
+}
+console.log('loop finished');
+
+const names= ['shaun', 'mario', 'luigi']; //original
+
+for( let i=0; i<names.length; i++){
+    //console.log(i);
+   //console.log(names[i]);
+   let html= `<div>${names[i]}</div>`;
+   console.log(html);
+}
+
+
+
+//while loop
+let i = 0;
+while( i < 5){
+    console.log('loop:', i);
+    i++;
+    
+}
+
+
+
+
+const names= ['shaun', 'mario', 'luigi'];//repeat 1
+let i = 0;
+while( i< names.length){
+    console.log(names[i]);
+    i++;
+}
+*/
+//do while loop (ensures the code runs atleast once even if the condition is false)
+
+let i = 5;
